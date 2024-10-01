31cb116f05cbf4b6f185264176c6c4bb39137609
imtiyazag <imtiyazagasimani@gmail.com>
2024-10-01 09:49:57 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+function func(){
+    let num1 = parseInt(document.getElementById('length').value);
+    let num2 = parseInt(document.getElementById('width').value);
+    var x = (num1 * num2)
+   document.getElementById('result').innerHTML = "The Area of Rectangle is "+x;
+   
+   
+   
+    
+}
